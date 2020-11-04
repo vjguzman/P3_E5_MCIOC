73592e4d6533ffb152ed7e0861a84d67d4345558
Veronica Guzman <48454058+vjguzman@users.noreply.github.com>
2020-11-04 00:30:06 -0300

    Casos + Generico

--- Caso_6.py
+++ Caso_6.py
@@ -1,4 +1,4 @@
-# Caso 5
+# Caso 6
 from caso_generico import caso_generico, gif, puntos
 
 # (a, b, Nx, Ny, c_in, c_sup, c_inf, c_izq, c_der, g_sup, g_inf, g_izq, g_der, caso)
@@ -9,11 +9,11 @@ Nx = 30                  # numero de intervalos en x
 Ny = 30                  # numero de intervalos en y
 
 # Condiciones de borde
-c_in = 5                 # T° inicial                 
+c_in = 30                # T° inicial                 
 c_sup = 0				 # Borde superior
 c_inf = 0                # Borde inferior
-c_izq = 25               # Borde izquierdo
-c_der = 25			     # Borde derecho
+c_izq = 10               # Borde izquierdo
+c_der = 0			     # Borde derecho
 
 # Gradientes
 g_sup = 0                # Gradiente superior
@@ -21,13 +21,13 @@ g_inf = 0                # Gradiente inferior
 g_izq = 0                # Gradiente izquierdo
 g_der = 0                # Gradiente derecho
 
-caso = "caso_5"          # Nombre del caso
+caso = "caso_6"          # Nombre del caso
 
 p1, p2, p3, sup = caso_generico(a, b, Nx, Ny, c_in, c_sup, c_inf, c_izq, c_der, g_sup, g_inf, g_izq, g_der, caso)
 
 # GIF
-fp_in = "caso_5/frame_*.png"
-fp_out = "caso_5.gif"
+fp_in = "caso_7/frame_*.png"
+fp_out = "caso_7.gif"
 gif(fp_in, fp_out)
 
 # Grafico Evolucion
--- caso_generico.py
+++ caso_generico.py
@@ -38,6 +38,7 @@ def truncate(n, decimals=0):
 
 # Funcion para los casos
 def caso_generico(a, b, Nx, Ny, c_in, c_sup, c_inf, c_izq, c_der, g_sup, g_inf, g_izq, g_der, caso):
+
     dx = b/Nx
     dy = a/Ny
 
@@ -46,6 +47,7 @@ def caso_generico(a, b, Nx, Ny, c_in, c_sup, c_inf, c_izq, c_der, g_sup, g_inf, 
     c = 450      # J/Kg*C
     rho = 7800   # Kg/m^3
     alpha = (K*dt)/(c*rho*(dx**2))
+
 
     # dx y dy tienen qye ser iguales!
     if dx != dy:
@@ -78,6 +80,14 @@ def caso_generico(a, b, Nx, Ny, c_in, c_sup, c_inf, c_izq, c_der, g_sup, g_inf, 
     T = 1*dia
     Days = 1*T # Cuantos dias quiero simular
 
+    # Graficar evolucion de temperatura en puntos importantes
+    
+    graf = np.int(Days/dt)
+    punto_1 = np.zeros(graf)
+    punto_2 = np.zeros(graf)
+    punto_3 = np.zeros(graf)
+    superficie = np.zeros(graf)
+
     # Loops en el tiempo
     for k in range(int32(Days/dt)):
         t = dt*(k+1)
@@ -87,11 +97,19 @@ def caso_generico(a, b, Nx, Ny, c_in, c_sup, c_inf, c_izq, c_der, g_sup, g_inf, 
         titulo = "k = {0:05.0f}".format(k) + " t = {0:02.0f}d {1:02.0f}h {2:02.0f}m ".format(dias,horas,minutos)
         
         # CB esenciales, se repiten en cada iteracion
-        u_k[0, :] = c_izq + g_izq*dx                             #izq
-        u_k[:, 0] = c_inf + g_inf*dx                           #inf
-        u_k[:, -1] = c_sup + g_sup*dx                          #sup
-        u_k[-1,:] = u_k[-2, :] + g_der*dx  + c_der    # der, gradiente 5
+        u_k[0, :] = c_izq + g_izq*dx                       #izq
+        u_k[:, 0] = c_inf + g_inf*dx                       #inf
+        u_k[:, -1] = c_sup + g_sup*dx                      #sup
+
+        if c_sup != 7:
+            u_k[-1,:] = u_k[-2, :] + g_der*dx              #der
         
+        if c_sup == 7:
+            seno = (2*int(int(3.14)/T)*t)
+            caso_superior = 20 + 10*np.sin(seno)
+            u_k[-1,:] = caso_superior
+
+
         # Loop en el espacio i = 1....n-1
         for i in range (1,Nx):
             for j in range(1,Ny):
@@ -110,7 +128,21 @@ def caso_generico(a, b, Nx, Ny, c_in, c_sup, c_inf, c_izq, c_der, g_sup, g_inf, 
         u_k[0, :] = c_izq + g_izq*dx                       #izq
         u_k[:, 0] = c_inf + g_inf*dx                       #inf
         u_k[:, -1] = c_sup + g_sup*dx                      #sup
-        u_k[-1,:] = u_k[-2, :] + g_der*dx                  # der, gradiente 5
+
+        if c_sup != 'caso7':
+            u_k[-1,:] = u_k[-2, :] + g_der*dx              #der
+        
+        if c_sup == 7:
+            seno = (2*int(int(3.14)/T)*t)
+            caso_superior = 20 + 10*np.sin(seno)
+            u_k[-1,:] = caso_superior
+
+
+        # Puntos para graficar evolución
+        punto_1[k] = u_k[int(Nx/2),int(Ny/2)]
+        punto_2[k] = u_k[int(Nx/2),int(3*Ny/4)]
+        punto_3[k] = u_k[int(3*Nx/4),int(3*Ny/4)]
+        superficie[k] = u_k[int(Nx/2),-1]
 
         #Grafico en d_next
         if t > next_t:
@@ -121,6 +153,9 @@ def caso_generico(a, b, Nx, Ny, c_in, c_sup, c_inf, c_izq, c_der, g_sup, g_inf, 
             framenum +=1
             next_t += dnext_t
             close(1)
+    
+    return punto_1, punto_2, punto_3, superficie
+
 
 # ---------------------------------
 import glob
@@ -141,3 +176,27 @@ def gif(fp_in, fp_out):
     print("listaImagenes: ", listaImagenes)
     img, *imgs = [Image.open(f) for f in listaImagenes]
     img.save(fp=fp_out, format="GIF", append_images=imgs, save_all=True, duration=150, loop=0)
+
+def puntos(punto_1, punto_2, punto_3, superficie, caso):
+    # Loop en el tiempo
+    minuto = 60
+    hora = 60*minuto
+    dia = 24*hora
+
+    dt = 1*minuto
+    dnext_t = 0.5*hora
+
+    next_t = 0
+    framenum = 0
+
+    T = 1*dia
+    Days = 1*T # Cuantos dias quiero simular
+
+    t_evolucion = np.arange(0, int(Days/dt), 1)
+    plot(t_evolucion, punto_1, label='Punto 1 -> (a/2, b/2)', color= 'orange')
+    plot(t_evolucion, punto_2, label='Punto 2 -> (a/2, 3b/4)', color= 'green')
+    plot(t_evolucion, punto_3, label='Punto 3 -> (3a/4, 3b/4)', color= "red")
+    plot(t_evolucion, superficie, label='Nivel de superficie', color='blue')
+    legend(loc="upper right")
+    title("Evolución de temperatura en puntos")
+    savefig(f"{caso}/Grafico_Evolucion_{caso}.png")
