4ecf24122f3722052479cc72829cfcf98754778c
alejandrobelloc <69279675+alejandrobelloc@users.noreply.github.com>
2020-11-05 11:11:56 -0300

    Update caso_4.py

--- caso4/caso_4.py
+++ caso4/caso_4.py
@@ -1,4 +1,4 @@
-#Caso 5
+#Caso 4
 
 from matplotlib.pylab import *
 #from matplotlib.pylab import cmap
@@ -7,8 +7,8 @@ import numpy as np
 # Definimos geometria
 
 a = 1 #alto
-b = 1 #ancho
-Nx = 30 #numero de intervalos en x
+b = 0.5 #ancho
+Nx = 15 #numero de intervalos en x
 Ny = 30 # numero de intervalos en y
 
 dx = b/Nx
@@ -27,34 +27,34 @@ x,y = coords(4,2)
 
 
 def imshowbien(u):
-	imshow(u.T[Nx::-1,:], cmap=cm.coolwarm)
-	cbar=colorbar(extend='both', cmap=cm.coolwarm)
-	ticks = arange(0,35,5)
-	ticks_Text=["{}".format(deg) for deg in ticks]
-	cbar.set_ticks(ticks)
-	cbar.set_ticklabels(ticks_Text)
-	clim(0,30)
+    imshow(u.T[Nx::-1,:], cmap=cm.coolwarm)
+    cbar=colorbar(extend='both', cmap=cm.coolwarm)
+    ticks = arange(0,35,5)
+    ticks_Text=["{}".format(deg) for deg in ticks]
+    cbar.set_ticks(ticks)
+    cbar.set_ticklabels(ticks_Text)
+    clim(0,30)
 
-	xlabel('b')
-	ylabel('a')
-	xTicks_N = arange(0,Nx+1,3)
-	yTicks_N = arange(0,Ny+1,3)
-	xTicks = [coords(i,0)[0] for i in xTicks_N]
-	yTicks = [coords(0,i)[1] for i in yTicks_N]
-	xTicks_Text=["{0:.2f}".format(tick) for tick in xTicks]
-	yTicks_Text=["{0:.2f}".format(tick) for tick in yTicks]
+    xlabel('b')
+    ylabel('a')
+    xTicks_N = arange(0,Nx+1,3)
+    yTicks_N = arange(0,Ny+1,3)
+    xTicks = [coords(i,0)[0] for i in xTicks_N]
+    yTicks = [coords(0,i)[1] for i in yTicks_N]
+    xTicks_Text=["{0:.2f}".format(tick) for tick in xTicks]
+    yTicks_Text=["{0:.2f}".format(tick) for tick in yTicks]
 
-	xticks(xTicks_N, xTicks_Text, rotation='vertical')
-	yticks(yTicks_N, yTicks_Text)
-	margins(0.2)
-	subplots_adjust(bottom=0.15)
+    xticks(xTicks_N, xTicks_Text, rotation='vertical')
+    yticks(yTicks_N, yTicks_Text)
+    margins(0.2)
+    subplots_adjust(bottom=0.15)
 
 
 u_k = zeros((Nx+1, Ny+1), dtype=double)
 u_km1 = zeros((Nx+1, Ny+1), dtype=double)
 
 # Condicion de borde inicial
-u_k[:,:] = 5 #en todas las celdas
+u_k[:,:] = 10 #en todas las celdas
 
 #Parametros
 dt = 0.01    # s
@@ -81,7 +81,6 @@ graf = np.int(Days/dt)
 punto_1 = np.zeros(graf)
 punto_2 = np.zeros(graf)
 punto_3 = np.zeros(graf)
-superficie = np.zeros(graf)
 
 def truncate(n, decimals=0):
     multiplier = 10**decimals
@@ -94,12 +93,12 @@ for k in range(int32(Days/dt)):
     horas = truncate((t-dias*dia)/hora,0)
     minutos = truncate((t-dias*dia - horas*hora)/minuto,0)
     titulo = "k = {0:05.0f}".format(k) + " t = {0:02.0f}d {1:02.0f}h {2:02.0f}m ".format(dias,horas,minutos)
-    
     # CB esenciales, se repiten en cada iteracion
-    u_k[0, :] = 25                      #izq
-    u_k[:, 0] = u_k[:,-2] + 0*dx        #inf
-    u_k[:, -1] = u_k[:,-2] + 0*dx       #sup
-    u_k[-1,:] = 25                      # der
+    
+    u_k[0, :] = 20                      #izq
+    u_k[:, 0] = 20                      #inf
+    u_k[:, -1] = 0                      #sup
+    u_k[-1,:] = u_k[-2,:] + 0*dx        # der
 
     
     # Loop en el espadio i = 1....n-1
@@ -118,23 +117,22 @@ for k in range(int32(Days/dt)):
     u_k = u_km1
     
     # CB denuevo para asegurar cumplimiento
-    u_k[0, :] = 25                      #izq
-    u_k[:, 0] = u_k[:,-2] + 0*dx        #inf
-    u_k[:, -1] = u_k[:,-2] + 0*dx       #sup
-    u_k[-1,:] = 25                      # der
+    u_k[0, :] = 20                      #izq
+    u_k[:, 0] = 20                      #inf
+    u_k[:, -1] = 0                      #sup
+    u_k[-1,:] = u_k[-2,:] + 0*dx        # der
 
     # Puntos para graficar evolución
-    punto_1[k] = u[int(Nx/2),int(Ny/2)]
-    punto_2[k] = u[int(Nx/2),int(3*Ny/4)]
-    punto_3[k] = u[int(3*Nx/4),int(3*Ny/4)]
-    superficie[k] = u[int(Nx/2),int(Ny/2)]
+    punto_1[k] = u_k[int(Nx/2),int(Ny/2)]
+    punto_2[k] = u_k[int(Nx/2),int(3*Ny/4)]
+    punto_3[k] = u_k[int(3*Nx/4),int(3*Ny/4)]
 
     #Grafico en d_next
-    if t > next_t:
+    if t >= next_t:
         figure(1)
         imshowbien(u_k)
         title(titulo)
-        savefig("caso5/frame_{0:04.0f}.png".format(framenum))
+        savefig("caso4/frame_{0:04.0f}.png".format(framenum))
         framenum +=1
         next_t += dnext_t
         close(1)
@@ -162,18 +160,19 @@ def gif(fp_in, fp_out):
     img.save(fp=fp_out, format="GIF", append_images=imgs, save_all=True, duration=150, loop=0)
 
 # GIF
-fp_in = "caso5/frame_*.png"
-fp_out = "caso5.gif"
+fp_in = "caso4/frame_*.png"
+fp_out = "caso4.gif"
 gif(fp_in, fp_out)
 
 # Grafico evolución de temperatura
 figure(2)
 t_evolucion = np.arange(0, int(Days/dt), 1)
+superficie = np.zeros(int(Days/dt))
 plot(t_evolucion, punto_1, label='N/4', color= 'orange')
 plot(t_evolucion, punto_2, label='2N/4', color= 'green')
 plot(t_evolucion, punto_3, label='3N/4', color= "red")
-plot(t_evolucion, superficie, label='superficie', color= "blue")
+plot(t_evolucion,superficie , label='superficie', color= "blue")
 legend(loc="upper right")
 title("Evolución de temperatura en puntos")
-savefig(f"caso5/Grafico_Evolucion_caso5.png")
+savefig(f"caso4/Grafico_Evolucion_caso2.png")
 show()
